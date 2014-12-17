42fc693f47ae4d1fc9bcceb25e6e07b2ba5211e6
Jcramos426 <jcramos426@gmail.com>
2014-12-17 17:55:43 -0500

    updating portfolio pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,131 @@
 $(document).ready(function() {
-	$('.image-link').magnificPopup({
+	$('.walk-link').magnificPopup({
+		type: 'image'
+	});
+	
+	$('.boss-gallery').magnificPopup({
+		items: [
+			{
+				src: '../img/portfolio/bad-boss/bad-boss-closed.png'
+			},
+			{
+				src: '../img/portfolio/bad-boss/bad-boss-open.png'
+			},
+			{
+				src: '../img/portfolio/bad-boss/bad-boss-series.png'
+			},
+			{
+				src: '../img/portfolio/bad-boss/bad-boss-password.png'
+			},
+			{
+				src: '../img/portfolio/bad-boss/bad-boss-documents.png'
+			},
+			{
+				src: '../img/portfolio/bad-boss/bad-boss-sketch1.png'
+			},
+			{
+				src: '../img/portfolio/bad-boss/bad-boss-sketch2.png'
+			},
+		],
+		gallery: {
+			enabled:true
+		}, 
+		type: 'image'
+	});
+	
+	$('.narrative-gallery').magnificPopup({
+		items: [
+			{
+				src: '../img/portfolio/narrative/cow-turkey-closed.png'
+			},
+			{
+				src: '../img/portfolio/narrative/cow-turkey-unwrap.png'
+			},
+			{
+				src: '../img/portfolio/narrative/cow-turkey-open.png'
+			},
+			{
+				src: '../img/portfolio/narrative/cow-turkey-title.png'
+			},
+			{
+				src: '../img/portfolio/narrative/cow-turkey-penultimate.png'
+			},
+			{
+				src: '../img/portfolio/narrative/cow-turkey-end.png'
+			},
+			{
+				src: '../img/portfolio/narrative/cow-turkey-sketch1.png'
+			},
+			{
+				src: '../img/portfolio/narrative/cow-turkey-sketch2.png'
+			},
+			{
+				src: '../img/portfolio/narrative/cow-turkey-sketch3.png'
+			},
+			{
+				src: '../img/portfolio/narrative/cow-turkey-sketch4.png'
+			},
+		],
+		gallery: {
+			enabled:true
+		}, 
+		type: 'image'
+	});
+	
+	$('.micagd-gallery').magnificPopup({
+		items: [
+			{
+				src: '../img/portfolio/micagd/micagd-original-home.png'
+			},
+			{
+				src: '../img/portfolio/micagd/micagd-original-hover.png'
+			},
+			{
+				src: '../img/portfolio/micagd/micagd-sketch.png'
+			},
+			{
+				src: '../img/portfolio/micagd/micagd-home.png'
+			},
+			{
+				src: '../img/portfolio/micagd/micagd-gallery.png'
+			},
+			{
+				src: '../img/portfolio/micagd/micagd-work.png'
+			},
+			{
+				src: '../img/portfolio/micagd/micagd-mobile.png'
+			},
+		],
+		gallery: {
+			enabled:true
+		}, 
+		type: 'image'
+	});
+	
+	$('.stamps-gallery').magnificPopup({
+		items: [
+			{
+				src: '../img/portfolio/stamps/stamps-sheet.png'
+			},
+			{
+				src: '../img/portfolio/stamps/stamps-celebrity.png'
+			},
+			{
+				src: '../img/portfolio/stamps/stamps-faith.png'
+			},
+			{
+				src: '../img/portfolio/stamps/stamps-politics.png'
+			},
+			{
+				src: '../img/portfolio/stamps/stamps-sketch1.png'
+			},
+			{
+				src: '../img/portfolio/stamps/stamps-sketch2.png'
+			},
+		],
+		gallery: {
+			enabled:true
+		}, 
 		type: 'image'
 	});
 });
